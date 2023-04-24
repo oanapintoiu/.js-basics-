b3b409af9b5f13f16c2fe0efef93a78fad6f0146
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-04-24 19:43:32 +0100

    part 10_arrays complete

---
+++ 10_objects/cohort.js
@@ -0,0 +1,7 @@
+const cohort = {
+    name: 'March 2023',
+    id: 2023,
+    peers: ['Arya', 'Q', 'Pablo', 'Oana']
+};
+
+console.log(cohort.id + ' - ' + cohort.name + ' cohort has ' + cohort.peers.length + ' students.');
---
+++ 10_objects/object_person_char.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Maxine',
+    age: 32,
+    address: 'London, E1 123'
+  };
+  
+  // both ways can be used to access an object's property:
+  console.log(person.name);
+  console.log(person['name']);
+
+  console.log(person.age);
+  console.log(person.address);
